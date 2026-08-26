8ef1e9c384b76b1b170694d0ceff57143336b4a4
Aditya bhat <aditya24.mitmpl2026@learner.manipal.edu>
2026-08-26 23:20:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.85%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -2,7 +2,7 @@ class Solution:
     def addDigits(self, num: int) -> int:
         n = str(num)
         
-        # Keep reducing as long as the string representation has more than 1 digit
+       
         while len(n) > 1:
             l = []
             for i in n:
@@ -12,6 +12,6 @@ class Solution:
             for a in range(len(l)):
                 total += int(l[a])
                 
-            n = str(total)  # Update n with the new sum to run again if needed
+            n = str(total)  
             
         return int(n)
